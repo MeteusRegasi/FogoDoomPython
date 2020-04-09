85f9596bd4f2f6dee2fb76b0ae505d8905424b6f
Mateus Regasi <mateusregasi123@gmail.com>
2020-04-09 01:23:39 -0300

    Criando fogo e sua propagação
    deu algum erro de indentação

--- comandos.py
+++ comandos.py
@@ -1,6 +1,26 @@
-def criarMatriz(alt, lar, pxt):
+from random import randint
+
+
+def criarMatriz(alt, lar, pxt=1):
     matriz = []
     for coluna in range(0, alt * pxt, pxt):
         for linha in range(0, lar * pxt, pxt):
             matriz.append((linha, coluna))
     return matriz
+def criarFogo(matriz):
+    fogo = {}
+    for pos in matriz:
+        if pos[1] == matriz[-1][1]:
+            fogo[pos] = 36
+        else:
+            fogo[pos] = 0
+    return fogo
+def fogoProp(fogo):
+    fogor = fogo
+    for pos, fi in fogor.items:
+        try:
+            fogor[pos] = fogor[(pos[0], pos[1] - 1)] - randint(0, 5)
+            if fogor[pos] < 0:
+                fogor[pos] = 0
+    print(fogor)
+    return fogor
--- principal.py
+++ principal.py
@@ -2,24 +2,31 @@ from pygame import *
 from comandos import *
 from time import clock
 
+
 #Definições da janela
-tamanhoJanela = altura, largura = 1000, 1000
+tamanhoJanela = altura, largura = 100, 100
 tela = display.set_mode(tamanhoJanela)
-init()
+#init()
 
 #Define o fps
 fps = clock()
 #Define o pixel
-pixel = (10, 10)
+pixel = (1, 1)
 
-matriz = criarMatriz(100, 100, 10)
-print(matriz)
+#Define as posições do fogo
+matriz = criarMatriz(10, 10, 1)
+#Coloca um valor de 0 a 36 em cada valor
+fogo = criarFogo(matriz)
 
+fogo = fogoProp(fogo)
+print(fogo)
+
+"""
 #Loop
 while True:
     #Aplica o fps
     fps
     #Loop de eventos
     for eventos in event.get():
-        if evento.type == QUIT:
-            exit
+        if eventos.type == QUIT:
+            exit"""
